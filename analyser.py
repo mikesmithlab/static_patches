~ sped up the force calculation by vectorising more ~ the force calculation is very slow so I will hopefully be able to avoid it ~ fixed an analyser indexing problem for charge plotting > need to make a decision about electrostatic forces > need to vectorise electrostatic forces function more still! > could move sphere_points_maker into conds dictionary?

analyser.py
@@ -73,8 +73,8 @@ def plot_charges(n):  # produces a plot of cumulative hits over time for every p
         charge_file = open("charges", "r")
     except FileNotFoundError:
         raise FileNotFoundError("You deleted the charges file or didn't make it with PatchTracker")
-    plot_length = int(1 + (len(charge_file.readlines()) - 1) / 3)  # todo check this is the right number
-    hit_time_list = np.zeros(plot_length)  # todo remove this for the charges, just use actual time!
+    plot_length = int(1 + (len(charge_file.readlines()) - 2) / 3)
+    time_list = np.zeros(plot_length)  # todo remove this for the charges, just use actual time!
     p_charge_list = np.zeros([plot_length, n])
     c_charge_list = np.zeros([plot_length, n])
     charge_file = open("charges", "r")  # todo use "with charge_file as open()"
@@ -83,13 +83,13 @@ def plot_charges(n):  # produces a plot of cumulative hits over time for every p
     for line in charge_file:
         if i >= 0:
             if i % 3 == 0:
-                hit_time_list[int(1 + i / 3)] = float(line)  # store the time of this collision
+                time_list[int(i / 3)] = float(line)  # store the time of this collision
             elif i % 3 == 1:
-                j = int(1 + (i - 1) / 3)
+                j = int((i - 1) / 3)
                 field = line.strip().split(",")
                 p_charge_list[j, :] = np.array(field)
             else:
-                j = int(1 + (i - 2) / 3)
+                j = int((i - 2) / 3)
                 field = line.strip().split(",")
                 c_charge_list[j, :] = np.array(field)
         i += 1
@@ -97,11 +97,11 @@ def plot_charges(n):  # produces a plot of cumulative hits over time for every p
 
     fig_pc = plt.figure()
     fig_pc.canvas.manager.set_window_title("Particle Charge")
-    plt.plot(hit_time_list, p_charge_list, hit_time_list, np.sum(p_charge_list, axis=1))  # include total plot
+    plt.plot(time_list, p_charge_list, time_list, np.sum(p_charge_list, axis=1))  # include total plot
     plt.xlabel("Time/s")
     plt.ylabel("Charge/nC")
     fig_cc = plt.figure()
     fig_cc.canvas.manager.set_window_title("Container Charge")
-    plt.plot(hit_time_list, c_charge_list, hit_time_list, np.sum(c_charge_list, axis=1))  # include total plot
+    plt.plot(time_list, c_charge_list, time_list, np.sum(c_charge_list, axis=1))  # include total plot
     plt.xlabel("Time/s")
     plt.ylabel("Charge/nC")

my_tools.py
@@ -52,13 +52,10 @@ def find_rotation_matrix(new_x, new_z):  # returns rotation matrix to rotate an 
 
 
 def find_in_new_coordinates(a, new_x, new_z):  # returns a in new coordinates given by new_x and new_z
-    # input a is a np.a of vectors, shaped like [n, 3]
+    # input a is a np.array of vectors, shaped like [n, 3]
     # credit for this function goes to
     # https://stackoverflow.com/questions/22081423#22081723
-    a_reshaped = a.reshape((a.shape[0], a.shape[1]))  # todo no idea if this is right, needs testing.
-    # a_reshaped = a.reshape((a.shape[0] * a.shape[1], a.shape[2]))  # original from source
-    return np.dot(find_rotation_matrix(new_x, new_z), a_reshaped.T).T.reshape(a.shape)
-    # return find_rotation_matrix(new_x, new_z).dot(a.reshape((a.shape[0] * a.shape[1], a.shape[2])).T).T.reshape(a.shape)
+    return np.dot(find_rotation_matrix(new_x, new_z), a.reshape((a.shape[0], a.shape[1])).T).T.reshape(a.shape)
 
 
 def sphere_points_maker(n, offset):  # returns n points on a unit sphere (roughly) evenly distributed
@@ -94,39 +91,37 @@ def find_tangent_force(normal_force, normal, surface_velocity, gamma_t, mu):  # 
     return tangent_direction.dot(-min(gamma_t * xi_dot, mu * find_magnitude(normal_force)))
 
 
-def find_electrostatic_forces(charges_part, charges_cont, points_part, tree_cont):  # returns forces on each patch
+def find_electrostatic_forces(charges_part, charges_cont, points_part, points_cont, tree_cont):  # returns forces on each patch
     # every patch needs to be matched with every patch
     # todo minimum distance: at the moment cont radius slightly larger, is this optimal?
     # todo 8.988e9 from wikipedia Coulomb's constant
     charge_forces = np.zeros(np.shape(points_part))
-    n_by_3 = np.shape(points_part)  # this is probably not the fastest way of doing it?  # todo
-    # todo vectorise this MOAR!
-    for i in range(n_by_3[0]):  # todo check? also check for speed
-        # magnitudes = (charges_part[i] * charges_cont * 8.988e9) * np.reciprocal(
-        #     tree_cont.query(points_part[i, :], k=n_by_3[0])[0] ** 2)  # todo non-numpy way? which is faster?
-        dists = np.array(tree_cont.query(points_part[i, :], k=n_by_3[0])[0])
-        print(dists)
-        magnitudes = (charges_part[i] * charges_cont * 8.988e9) * np.reciprocal(dists.dot(dists))
-        # todo KDTree query has "workers" for parallel processing! is it worth it here?
-        directions = np.zeros(n_by_3)
-        charge_forces[i, :] = np.sum(directions * magnitudes, 0)  # todo don't put direction and magnitude in memory!
+    n = np.shape(charge_forces)[0]
+    all_dists = np.array(tree_cont.query(points_part, k=n)[0])
+    # todo vectorise this more?
+    for i in range(n):  # todo check? also check for speed
+        difference = points_cont - points_part[i, :]
+        charge_forces[i, :] = np.sum(
+            difference * np.reciprocal(np.sum(np.square(difference), axis=1) ** 0.5)[:, np.newaxis] *
+            ((charges_part[i] * charges_cont * 8.988e9) * np.reciprocal(np.square(all_dists[:, i])))[:, np.newaxis],
+            axis=0
+        )
     # charge_forces is the net force on each individual patch on part
     return charge_forces
 
 
-def charge_decay_function(charge, decay, shift=0):  # returns the new charges due to decay (to air?)
-    charge = charge - shift
+def charge_decay_function(charge, decay_exponential, shift=0):  # returns the new charges due to decay (to air?)
     # todo does the charge spread to nearby patches? <-- would be horrible to compute
-    return charge.dot(decay) + shift
+    return (charge - shift).dot(decay_exponential) + shift
 
 
-def charge_hit_function(patch_charge_part, patch_charge_cont):  # returns the new charges of colliding patches
+def charge_hit_function(patch_charge_part, patch_charge_cont, charge_per_hit):  # returns the new charges of colliding patches
     # todo:
     # do previous charges of the patches matter? or just add some constant every collision? ('proper "saturation"'?)
     # does the force matter?
     # does the charge of nearby patches matter?
     # constant that is added needs to change with patch area (work area out once then input it to this function)
-    return patch_charge_part + 4e-13, patch_charge_cont - 4e-13
+    return patch_charge_part + charge_per_hit, patch_charge_cont - charge_per_hit
 
 
 def round_sig_figs(x, p):  # credit for this significant figures function https://stackoverflow.com/revisions/59888924/2

physics.py
@@ -15,7 +15,7 @@ class PatchTracker:
     def __init__(self, n, offset, r_part, r_cont):
         self.points = sphere_points_maker(n, offset)  # todo separate n for part and cont?
         self.points_part = self.points.dot(r_part)
-        self.points_cont = self.points.dot(r_cont + r_part * 0.05)  # todo make sure to keep track of the plus 0.0r_part
+        self.points_cont = self.points.dot(r_cont + r_part * 0.01)  # todo make sure to keep track of the plus 0.0r_part
         self.tree = KDTree(self.points)  # input to KDTree for 3D should have dimensions (n, 3)
         self.tree_cont = KDTree(self.points_cont)  # todo uses plus 0.0r_part
 
@@ -29,11 +29,11 @@ class PatchTracker:
             "Format: 3 alternating lines, first is time of collision, second is particle patches, "
             "third is container patches"
         )
-        self.charges_part = np.zeros(n)
-        self.charges_cont = np.zeros(n)
-
+        self.charges_part = np.ones(n) * (0 / n)  # (-1e-9 / n)
+        self.charges_cont = np.ones(n) * (0 / n)  # starting charge
+        self.charge_per_hit = 4e-13
         self.decay_constant = -0.01  # approximate decay constant from experimental data. Half life is approx 11.5 mins
-        self.decay_to_charge = 0.38 * 1e-9 / n  # from experimental data????????
+        self.decay_to_charge = 0  # 0.38 * 1e-9 / n  # from experimental data
 
     def collision_update(self, t, pos, particle_x, particle_z):  # input pos: normalised position relative to container
         # find the indexes of the patches that collided on the particle and container
@@ -46,7 +46,8 @@ class PatchTracker:
 
         # charge tracking for physics
         self.charges_part[part], self.charges_cont[cont] = charge_hit_function(self.charges_part[part],
-                                                                               self.charges_cont[cont])
+                                                                               self.charges_cont[cont],
+                                                                               self.charge_per_hit)
 
     def close(self):
         self.patches_file.close()
@@ -98,6 +99,8 @@ class Particle:
         self.force_multiplier = step / (2 * self.mass)
         self.torque_multiplier = step / (2 * self.moment_of_inertia)
         self.gravity_force = np.array([0, 0, g]).dot(self.mass)
+        self.electrostatic_force = np.array([0, 0, 0])
+        self.electrostatic_torque = np.array([0, 0, 0])
 
     def find_energy(self, overlap):  # returns the energy of the particle
         return (-self.gravity_force.dot(self.pos) +
@@ -163,11 +166,18 @@ class Engine:
             else:  # this else exists for speed - the code runs about 10% faster when overlap isn't assigned in update!
                 force, torque, _ = self.update(time, True)
             self.p.integrate_half(self.time_step, force, torque, True)
-            force, torque, _ = self.update(time, False)
+            # if i % 100 == 0:  # only do electro every 10
+            if False:
+                force, torque, _ = self.update(time, False, electro=True)
+                ratio = find_magnitude(self.p.electrostatic_force) / find_magnitude(force)
+                if ratio >= 1e-2:  # todo this is checking size print, remove soon please and thanks
+                    print(f"{ratio = }")
+            else:
+                force, torque, _ = self.update(time, False)
             self.p.integrate_half(self.time_step, force, torque, False)
         self.close()
 
-    def update(self, t, first_call):  # returns force and torque, also updates distances and patches
+    def update(self, t, first_call, electro=False):  # returns force and torque, also updates distances and patches
         # ----------------
         # charge decay/spreading
         self.p_t.charges_part = charge_decay_function(self.p_t.charges_part, self.decay, shift=self.p_t.decay_to_charge)
@@ -178,20 +188,20 @@ class Engine:
         overlap = self.radii_difference - find_magnitude(relative_pos)
         # ----------------
         # charge forces
-        electrostatic_forces = find_electrostatic_forces(self.p_t.charges_part, self.p_t.charges_cont,
-                                                         find_in_new_coordinates(self.p_t.points_part, self.p.x_axis,
-                                                                                 self.p.z_axis) + relative_pos,
-                                                         self.p_t.tree_cont)
-        # todo move these so we don't use memory to make them?
-        electrostatic_force = np.sum(electrostatic_forces, axis=0)  # todo is axis 0?
-        electrostatic_torque = np.array([0, 0, 0])  # todo turn into overall torque
+        if electro:
+            electrostatic_forces = find_electrostatic_forces(self.p_t.charges_part, self.p_t.charges_cont,
+                                                             find_in_new_coordinates(
+                                                                 self.p_t.points_part, self.p.x_axis, self.p.z_axis
+                                                             ) + relative_pos, self.p_t.points_cont, self.p_t.tree_cont)
+            self.p.electrostatic_force = np.sum(electrostatic_forces, axis=0)  # todo is axis 0?
+            self.p.electrostatic_torque = np.array([0, 0, 0])  # todo turn into overall torque
         # ----------------
         # check for contact
         if overlap >= 0:  # overlap is the distance the particle is inside the container wall (is >= 0 if not inside)
             self.contact = False
             if overlap >= self.p.radius * 0.001:
                 self.is_new_collision = True
-            return self.p.gravity_force + electrostatic_force, np.array([0, 0, 0]), 0  # return 0 overlap so find_energy doesn't need logic
+            return self.p.gravity_force + self.p.electrostatic_force, self.p.electrostatic_torque, 0  # return 0 overlap so find_energy doesn't need logic
         self.contact = True
         normal = normalise(relative_pos)
         # ----------------
@@ -210,9 +220,9 @@ class Engine:
             self.p_t.collision_update(t, normal, self.p.x_axis, self.p.z_axis)
             self.is_new_collision = False
             # make sure the next calls don't update the patches unless it is a new collision
-            # todo this biases the first patch touched (bias direction -avg_angular_velocity)
-        return self.p.gravity_force + normal_contact_force + tangent_contact_force, my_cross(
-            normal.dot(self.p.radius), tangent_contact_force), overlap
+            # todo this biases the first patch touched (bias direction -avg_angular_velocity
+        return self.p.gravity_force + normal_contact_force + tangent_contact_force + self.p.electrostatic_force, my_cross(
+            normal.dot(self.p.radius), tangent_contact_force) + self.p.electrostatic_torque, overlap
 
     def store(self, j, t, overlap):  # stores anything that needs storing this step in the data_dump file
         self.data_file.writelines(